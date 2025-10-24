ad2b449a36dc4c2cc9441e8badb012ea648dc770
Nashtech <mansi.jain1@nashtechglobal.com>
2025-10-24 16:59:48 +0530

    Pre-Composed Supergraph schema

--- Day2_Federations/src/gateway-subgraph.js
+++ Day2_Federations/src/gateway-subgraph.js
@@ -1,12 +1,19 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { ApolloGateway } from '@apollo/gateway';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load the pre-composed supergraph schema
+const supergraphPath = path.join(__dirname, '../src/schema/supergraph-schema.graphql');
+const supergraphSdl = fs.readFileSync(supergraphPath, 'utf-8');
 
 const gateway = new ApolloGateway({
-  serviceList: [
-    { name: 'users', url: 'http://localhost:4001' },
-    { name: 'posts', url: 'http://localhost:4002' },
-  ],
+  supergraphSdl,
 });
 
 const server = new ApolloServer({
